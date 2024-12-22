15ad3d26beda8994ec2e763947a42ac2fc048b14
Lalit Bhanot <76906227+lalitbhanot@users.noreply.github.com>
2024-12-22 17:24:43 +0100

    Initial Setup for project

---
+++ src/main/java/com/example/springframework/mssc_brewery/MsscBreweryApplication.java
@@ -0,0 +1,13 @@
+package com.example.springframework.mssc_brewery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscBreweryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsscBreweryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springframework/mssc_brewery/MsscBreweryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springframework.mssc_brewery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBreweryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
